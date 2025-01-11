72f8f5ce65666cb1fa20ab18fb454c372dabdc45
Sevakian <sevakian08@gmail.com>
2025-01-11 19:26:19 +0100

    feat: add database seeder for calendar, calendarDate & calendarUsage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,15 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Calendar;
+use App\Models\CalendarDate;
+use App\Models\CalendarUsage;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
@@ -19,5 +18,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        CalendarUsage::factory(5)->create();
+        Calendar::factory()
+            ->has(CalendarDate::factory(10))
+            ->create();
     }
 }
