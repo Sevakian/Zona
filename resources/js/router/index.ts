8f80b68a97f69f4ffebdc30d54d4749526cfe188
Sevakian <sevakian08@gmail.com>
2025-01-12 09:20:02 +0100

    feat: add basic calendar page

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import CalendarView from '@/views/CalendarView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: CalendarView,
   },
 ];
 
